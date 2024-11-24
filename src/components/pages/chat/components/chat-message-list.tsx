9dc0c91ca9ff5e79edb08f8f3ca10760663cbdc5
dean <bernarddean.cuyco@intellisolution.tech>
2024-11-24 19:36:24 +0800

    fix lint

--- src/components/pages/chat/components/chat-message-list.tsx
+++ src/components/pages/chat/components/chat-message-list.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { cn } from "@/lib/utils";
-import { useLayoutEffect, useMemo, useRef } from "react"
+import { useLayoutEffect, useRef } from "react"
 
 interface ChatMessageListProps {
   messages: { id: string; username: string; text: string; createdAt: string; sessionId:string }[] // Add sessionId to each message
@@ -29,9 +29,7 @@ export default function ChatMessageList({
             className={cn(`flex flex-col space-y-1`)}
             style={{ alignItems: msg.sessionId === currentSessionId ? 'flex-end' : 'flex-start' }}
           >
-            {msg.sessionId !== currentSessionId && (
-              <span className="text-gray-600 text-xs font-medium ml-1">{msg.username}</span>
-            )}
+            <span className="text-gray-600 text-xs font-medium ml-1">{`${msg.username}${msg.sessionId === currentSessionId ? ' (You)':''}`}</span>
             <div
               style={{
                 background: msg.sessionId === currentSessionId ? '#0089FF' : 'white',
